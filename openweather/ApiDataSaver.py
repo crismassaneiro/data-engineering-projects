df05154100269f8457f20ad3311fbb7483bba65e
Cristian Massaneiro <cristianworc@gmail.com>
2024-01-11 17:08:55 -0300

    saving data

--- openweather/ApiDataSaver.py
+++ openweather/ApiDataSaver.py
@@ -1,13 +1,2 @@
 class ApiDataSaver():
 
-    def catuaba(x = int, y = int):
-        x = 10
-        i = 2
-        for i in range(x):
-            print("peido gigante")
-        return i
-
-    def coriti():
-        print("peida sauve {i} ")    
-
-
