7ee08c92040021706ac5ce6cfb6a526f48b47bd1
Cristian Massaneiro <cristianworc@gmail.com>
2024-01-12 21:14:18 -0300

    creating functions

--- openweather/ApiDataSaver.py
+++ openweather/ApiDataSaver.py
@@ -1,14 +1,26 @@
+import os
+import json
+import boto3
+import requests
+import pandas as pd
+import pyarrow as pa
+import awswrangler as wr
+import pyarrow.parquet as pq
+from datetime import datetime
+
 class ApiDataSaver():
 
-# Criar um cliente para o AWS Secrets Manager com base no perfil
-client = boto3.session.Session(profile_name='CRISTIAN_AWS').client('secretsmanager', region_name='us-east-1')
+    def token_api(self):
+        client = boto3.session.Session(profile_name='CRISTIAN_AWS').client('secretsmanager', region_name='us-east-1')
+        response = client.get_secret_value(SecretId='env/openweather')
+        secret_data = response['SecretString']
+        secrets_dict = json.loads(secret_data)
+        open_weather_token = secrets_dict.get('OPEN_WEATHER_SECRET', '')  # Correção aqui
+        API_LINK = f"https://api.openweathermap.org/data/2.5/weather?q=novo hamburgo,br&APPID={open_weather_token}"
+        return API_LINK
 
-# Recuperar o segredo
-response = client.get_secret_value(SecretId='env/openweather')
-secret_data = response['SecretString']
-
-# Analisar o conteúdo do segredo (assumindo que é um formato JSON)
-secrets_dict = json.loads(secret_data)
-
-# Obtenha o valor associado à chave 'OPEN_WEATHER_SECRECT'
-open_weather_token = secrets_dict.get('OPEN_WEATHER_SECRECT', '')
+    def requisicao(self, API_LINK):
+        requisicao = requests.get(API_LINK)
+        wheater_json = requisicao.json()
+        weather_json_string = json.dumps(wheater_json)
+        return weather_json_string
